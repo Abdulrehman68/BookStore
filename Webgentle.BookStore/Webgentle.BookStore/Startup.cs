77dbef553e09346893a79d437bc9c1c9175afa8b
mani rock <manirock623@outlook.com>
2021-08-02 17:21:38 +0500

    Added to MVC

--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Webgentle.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,69 @@ namespace Webgentle.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async(context, next)=>
+            //{
+            //    await context.Response.WriteAsync("Hello my First middlewear!");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello my First middlewear response!");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello my Second middlewear!");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello my Second middlewear response!");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello my Third middlewear!");
+            //    await next();
+            //});
+
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapDefaultControllerRoute();
+
+               /* endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
-                });
+/*
+                            
+                       // For environment variable check       
+                    if (env.IsDevelopment())
+                    {
+                        await context.Response.WriteAsync("Hello developer!");
+                    }
+                    else if (env.IsEnvironment("Develop"))
+                    {
+                        await context.Response.WriteAsync("Hello Custome Name!");
+                    }
+                    else if (env.IsProduction())
+                    {
+                        await context.Response.WriteAsync("Hello PRO!");
+                    }
+                    else if (env.IsStaging())
+                    {
+                        await context.Response.WriteAsync("Hello Stage!");
+                    }
+                    else
+                    await context.Response.WriteAsync(env.EnvironmentName); 
+                
+                    
+                    }); */
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/ar", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello ar!");
+            //    });
+            //});
         }
     }
 }
